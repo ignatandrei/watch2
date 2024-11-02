e9be0189a07cd501d5bcc631b73bf177080fbea9
Andrei Ignat <ignatandrei@yahoo.com>
2024-11-02 21:21:40 +0200

    added di with nlog

--- src/Watch2/Watch2/Program.cs
+++ src/Watch2/Watch2/Program.cs
@@ -1,16 +1,50 @@
-using Watch2;
+//IConsoleWrapper console = new ConsoleWrapper();
+//var processManager = new ProcessManager();
+//IProcessStartInfo startInfo = new ProcessStartInfoWrapper
+//{
+//    FileName = "dotnet",
+//    Arguments = "watch " + string.Join(' ', args),
+//    WorkingDirectory = Environment.CurrentDirectory,
+//    RedirectStandardOutput = true,
+//    RedirectStandardInput = true,
+//    RedirectStandardError = true,
+//    UseShellExecute = false,
+//    CreateNoWindow = true
+//};
+//await processManager.StartProcessAsync(args, console,startInfo);
 
-IConsoleWrapper console = new ConsoleWrapper();
-var processManager = new ProcessManager();
-IProcessStartInfo startInfo = new ProcessStartInfoWrapper
+
+var serviceCollection = new ServiceCollection();
+ConfigureServices(serviceCollection);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var console = serviceProvider.GetRequiredService<IConsoleWrapper>();
+var processManager = serviceProvider.GetRequiredService<ProcessManager>();
+var startInfo = serviceProvider.GetRequiredService<IProcessStartInfo>();
+
+await processManager.StartProcessAsync(args, console, startInfo);
+
+void ConfigureServices(IServiceCollection services)
 {
-    FileName = "dotnet",
-    Arguments = "watch " + string.Join(' ', args),
-    WorkingDirectory = Environment.CurrentDirectory,
-    RedirectStandardOutput = true,
-    RedirectStandardInput = true,
-    RedirectStandardError = true,
-    UseShellExecute = false,
-    CreateNoWindow = true
-};
-await processManager.StartProcessAsync(args, console,startInfo);
+services.AddSingleton<IConsoleWrapper, ConsoleWrapper>();
+services.AddSingleton<ProcessManager, ProcessManager>();
+services.AddSingleton<IProcessStartInfo>(provider => new ProcessStartInfoWrapper
+{
+FileName = "dotnet",
+Arguments = "watch " + string.Join(' ', args),
+WorkingDirectory = Environment.CurrentDirectory,
+RedirectStandardOutput = true,
+RedirectStandardInput = true,
+RedirectStandardError = true,
+UseShellExecute = false,
+CreateNoWindow = true
+});
+
+    services.AddLogging(loggingBuilder =>
+    {
+        loggingBuilder.ClearProviders();
+        loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+        loggingBuilder.AddNLog("nlog.config");
+    });
+}
